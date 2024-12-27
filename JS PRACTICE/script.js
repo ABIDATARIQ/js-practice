3bccea5c41ed63274c8209660ae1b348d25bf3bb
ABIDATARIQ <162459792+ABIDATARIQ@users.noreply.github.com>
2024-12-27 15:56:27 +0500

    Add files via upload

---
+++ JS PRACTICE/script.js
@@ -0,0 +1,39 @@
+let companies= ["Bloomberg","Microsoft", "uber", "Google", "IBM","Netflex"];
+companies.pop();
+
+companies.push("careem");
+console.log(companies);
+
+let companie= ["Bloomberg","Microsoft", "uber", "Google", "IBM","Netflex"];
+companie.shift();
+companies.splice(1,1,"ola");
+console.log(companie);
+
+
+let fooditems = ["Apple","Potato","Peach","Orange"];
+console.log(fooditems);
+
+let colour = ["yellow","green","pink","blue","red"];
+let newcolour = colour.toString();
+console.log(colour);
+colour.unshift("brown");
+console.log(colour);
+
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.splice(2, 1, "Lemon", "Kiwi");
+console.log(fruits);
+
+let fruit = ["Banana", "Orange", "Apple", "Mango"];
+fruit.shift();
+console.log(fruit);
+
+const fruitss = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruitss.toString();
+console.log(fruitss);
+
+
+
+
